df264a81c091c626d0d135fdd74441050dc1cac4
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-02-12 11:12:57 -0800

    Setup default domain name

--- root/startup.sh
+++ root/startup.sh
@@ -2,6 +2,14 @@
 
 trap "killall sleep dnsmasq; exit" TERM INT
 
+cat > /etc/dnsmasq.conf <<__EOF__
+user=root
+no-resolv
+domain=${__DOMAINNAME}
+expand-hosts
+hostsdir=/etc/dnshosts.d
+__EOF__
+
 dnsmasq
 
 sleep 2147483647d &
